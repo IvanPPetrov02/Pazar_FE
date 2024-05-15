d8dcbde0cb498f350c76cdfc0ae06e2a53030a82
Ivan Petrov <ivan.p.petrov02@gmail.com>
2024-05-15 22:33:23 +0300

    change http to https

--- Pazar/src/Components/AxiosImport.jsx
+++ Pazar/src/Components/AxiosImport.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080',  // Ensure this matches your API URL
+    baseURL: 'https://localhost:8080',  // Ensure this matches your API URL
     withCredentials: true,  // Include credentials if necessary
     headers: {
         'Content-Type': 'application/json',
