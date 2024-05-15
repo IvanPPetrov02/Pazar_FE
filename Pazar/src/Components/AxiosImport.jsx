4bab3f4b610580d73c44607b0626e9220b68d632
Ivan Petrov <ivan.p.petrov02@gmail.com>
2024-05-15 16:29:27 +0300

    test pass?

--- Pazar/src/Components/AxiosImport.jsx
+++ Pazar/src/Components/AxiosImport.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://pazarapi:8000',  // Ensure this matches your API URL
+    baseURL: 'https://pazarapi-1:8000',  // Ensure this matches your API URL
     withCredentials: true,  // Include credentials if necessary
     headers: {
         'Content-Type': 'application/json',
